53b464e3724e8177de00cf0aa7be938e77a7c874
vamshi5421 <75656830+vamshi5421@users.noreply.github.com>
2021-11-11 01:39:36 +0530

    first commit

---
+++ redux.js
@@ -0,0 +1,43 @@
+const redux = require("redux");
+const createStore  = redux.createStore;
+
+
+const initialState = {
+    counter : 0
+}
+
+const rootReducer = (state = initialState,action) => {
+    if(action.type=== "INC_COUNTER"){
+        return {
+            ...state,
+            counter : state.counter + 1
+        }
+    }
+    if (action.type === 'ADD_COUNTER') {
+        return {
+          ...state,
+          counter: state.counter + action.payload.value,
+        };
+      }
+    return state
+}
+
+// const store = createStore(will take reducer)
+
+const store = createStore(rootReducer);
+console.log(store.getState());
+
+store.dispatch({type : "INC_COUNTER"});
+store.dispatch({type : "ADD_COUNTER", payload: 
+    {value : 10 }
+});
+console.log(store.getState());
+
+
+
+
+
+
+
+
+
